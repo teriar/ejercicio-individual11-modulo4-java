809c657ab526ceb4be0670a852e7c1cd665e59cb
Felipe Gonzalez <felipegonzalezurr@gmail.com>
2023-06-28 20:12:46 -0400

    agregado andy

--- app/src/main/java/com/example/cl/ejercicio11javaandroidmodulo4/MainActivity.java
+++ app/src/main/java/com/example/cl/ejercicio11javaandroidmodulo4/MainActivity.java
@@ -21,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
 
+        initListener();
+
+
+
+
+
+    }
+
+    private void initListener() {
+
         binding.btnAvion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,5 +38,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+       binding.ActualizarBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getBaseContext(),"Boton Actualizar presionado",Toast.LENGTH_SHORT).show();
+           }
+       });
+
+       binding.buttonPictureGallery.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getBaseContext(),"Boton Galeria Presionada",Toast.LENGTH_SHORT).show();
+           }
+       });
+
+       binding.buttonUnlockedLevel5.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getBaseContext(),"Desbloqueado nivel 5",Toast.LENGTH_SHORT).show();
+           }
+       });
+
+       binding.buttonLevel4.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getBaseContext(),"Boton nivel 4 presionado",Toast.LENGTH_SHORT).show();
+           }
+       });
+
+       binding.buttonMusicPaused.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               Toast.makeText(getBaseContext(),"Musica pausada",Toast.LENGTH_SHORT).show();
+           }
+       });
     }
 }
